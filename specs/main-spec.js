8f676378177100b6740fb8351d21ef88b255bc97
zaioll <zaioll@protonmail.com>
2020-10-12 19:37:04 -0400

    Instala ferramentas de tests

---
+++ specs/main-spec.js
@@ -0,0 +1,9 @@
+import assert from 'assert';
+
+describe('Test simples', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.strictEqual(-1, -1);
+    });
+  });
+});
